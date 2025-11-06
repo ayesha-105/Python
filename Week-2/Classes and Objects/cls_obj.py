e176bfdea2f6c8a2dabb0a73cc41fe13fab1ab24
Ayesha Malik <ayeshamalik4a1@gmail.com>
2025-11-06 18:19:44 +0500

    completed classes and objects

--- Week-2/Classes and Objects/cls_obj.py
+++ Week-2/Classes and Objects/cls_obj.py
@@ -6,11 +6,12 @@ class student:
 s1= student()
 s2= student()
 print(s1.uniname)  # We acces our class variable using object
-del s2   #deleting object s2
+
+#deleting object s2
+del s2  
 print(s2.uniname)  # this will give error as s2 is deleted
 
+# class cannot be empty but if we want to create empty class for some reason, we use pass statemnt to avoid error
 class Person:
     pass
 
-p1= Person()
-print(p1)
